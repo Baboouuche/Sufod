6b2c290014d364a7e6fb74b8caf772e09aa68914
bbeylerian <benoit.beylerian@imerir.com>
2022-04-25 16:25:39 +0200

    Compte/Admin/Joueur modif again

--- src/main/java/entity/Joueur.java
+++ src/main/java/entity/Joueur.java
@@ -1,14 +1,22 @@
 package entity;
 
 import java.util.List;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "joueur")
+@SequenceGenerator(name = "seqPersonne", sequenceName = "seq_joueur", allocationSize = 1)
 public class Joueur extends Compte{
 
-	public Joueur(int id, String nom, String prenom, String mail, String pseudo, List<Personnage> Personnage) {
-		super(id, nom, prenom, mail, pseudo, Personnage);
+	public Joueur(Long id, String nom, String prenom, String mail, String pseudo, Set<Personnage> personnages) {
+		super(id, nom, prenom, mail, pseudo, personnages);
 	}
 	
-	public Joueur(int id, String nom, String prenom, String mail, String pseudo) {
+	public Joueur(Long id, String nom, String prenom, String mail, String pseudo) {
 		super(id, nom, prenom, mail, pseudo);
 	}
 	
